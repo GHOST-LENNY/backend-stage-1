43fc6ae714be75162c96be7ff14ad1a45f811f80
lenny <lennyddon@gmail.com>
2023-09-09 06:16:24 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 7777;
+
+app.get("/api", (req, res) => {
+  const { slack_name, track } = req.query;
+
+  if (!slack_name && !track) {
+    res.status(404).json({ error: "slack_name and track parameters required" });
+  }
+
+
+  const date = new Date();
+  //get current date in required utc format
+  const currentDate = date.toISOString().slice(0, 19) + "Z";
+
+  const currentDay = date.toLocaleString('en-US', { weekday: 'long' });
+  const gitHubFileUrl = "";
+  const gitHubRepo = "";
+
+  const responseData = {
+    "slack_name": slack_name,
+    "current_day": currentDay,
+    "utc_time": currentDate,
+   "track": track,
+    "github_file_url": gitHubFileUrl,
+    "github_repo_url": gitHubRepo,
+    "status_code": 200,
+  };
+
+  res.json(responseData);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+  console.log(res.json(responseData));
+});
